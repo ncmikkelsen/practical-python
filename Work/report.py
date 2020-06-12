ad8b9bc94a8a7531c7ebb8695911778584fb4ad4
Nikolaj Mikkelsen <nikolaj.c.mikkelsen@gmail.com>
2020-06-12 15:44:43 +0200

    Complete exerise 2.7

--- Work/report.py
+++ Work/report.py
@@ -4,11 +4,12 @@
 
 import csv
 import sys
+from pprint import pprint
 
 def read_portfolio(filename):
     portfolio = []
 
-    with open(filename) as f:
+    with open(filename, 'rt') as f:
         rows = csv.reader(f)
         headers = next(rows)
         for row in rows:
@@ -24,10 +25,58 @@ def read_portfolio(filename):
 
     return portfolio
 
-if len(sys.argv) == 2:
-    filename = sys.argv[1]
-else:
-    filename = 'Data/portfolio.csv'
+def read_prices(filename):
+    prices = { }
 
-portfolio_list = read_portfolio(filename)
-print(portfolio_list)
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        for row in rows:
+            if len(row) == 2:
+                try:
+                    name = row[0]
+                    price = float(row[1])
+                    prices[name] = price
+                except ValueError:
+                    print('Error parsing', row)
+
+
+
+    return prices
+
+portfolio = read_portfolio('Data/portfolio.csv')
+prices = read_prices('Data/prices.csv')
+
+gain = 0
+total_cost = 0
+total_value = 0
+for h in portfolio:
+    name = h['name']
+    if name in prices:
+        o_price = h['price']
+        shares = h['shares']
+        c_price = prices[name]
+        this_gain = shares * (c_price - o_price)
+        gain += this_gain
+        #print(f"The gain/loss of {name} \t is ${this_gain:0.02f}")
+
+        print("The result of", name, end=' ')
+        if this_gain > 0.0:
+            print(f"is a gain of ${this_gain}")
+        else:
+            print(f"is a loss of ${this_gain}")
+        total_cost = shares * o_price
+        total_value = shares * c_price
+
+
+    else:
+        print(f"the name {name} is not in the pricelist")
+
+print()
+print(f'The total cost was ${total_cost:0.02f}')
+print(f'The total value is ${total_value:0.02f}')
+print('The result is a', end=' ')
+if gain > 0:
+    print('gain', end = ' ')
+else:
+    print('loss', end = ' ')
+print(f'of ${gain:0.02f}')
